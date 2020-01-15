c10ca62c03002eb691c4354a38e87ebf1d611ec1
Sabbir Hossain <smac@labs-mac.local>
2020-01-15 17:10:03 +0600

    Showing Data

--- Carthage IOS/ViewController.swift
+++ Carthage IOS/ViewController.swift
@@ -22,9 +22,21 @@ class ViewController: UIViewController {
 
     @IBAction func btn(_ sender: Any) {
         
+
+       
+
+        //Alamofire.request(.GET, "", encoding: .URL)
+        Alamofire.request("https://api.darksky.net/forecast/5c74c06e261c64b3a2f6e1abfb7fe85d/37.8267,-122.4233").responseJSON { response in
+            print(response.request as Any)  // original URL request
+            print(response.response as Any) // URL response
+            print(response.result.value as Any)   // result of response serialization
+            
+            self.txtview.text = response.description
+        }
         
-        
+       
     }
-    
+
 }
+
 

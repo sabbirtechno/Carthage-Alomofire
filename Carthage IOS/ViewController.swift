a7a9f907205a99cb97d31d88a9bae9a0e719b248
Sabbir Hossain <smac@labs-mac.local>
2020-01-21 16:15:35 +0600

    Task 3 Completed. Adding layout

--- Carthage IOS/ViewController.swift
+++ Carthage IOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var txtview: UITextView!
     
+    @IBOutlet weak var textview: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,6 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btn(_ sender: Any) {
-        
-
-       
-
         //Alamofire.request(.GET, "", encoding: .URL)
         
         Alamofire.request("https://api.darksky.net/forecast/5c74c06e261c64b3a2f6e1abfb7fe85d/37.8267,-122.4233").responseJSON { response in
@@ -36,6 +33,7 @@ class ViewController: UIViewController {
             if let jsonDictionary = response.result.value as? [String : Any] {
                 if let currentWeatherDictionary = jsonDictionary["currently"] as? [String : Any]{
                     let summary = currentWeatherDictionary["summary"] as? String
+                    
                     temp = currentWeatherDictionary["temperature"] as? Double ?? 0.0
                     
                     //Below code for converting double type too string
@@ -54,15 +52,44 @@ class ViewController: UIViewController {
                     dateFormatter.dateFormat = "dd-MM-yyyy HH:mm a" //Specify your format that you want //"dd-MM-yyyy HH:mm"
                     let strDate = dateFormatter.string(from: date as Date)
                     
-                  //  let date = "09:53 AM
-                   // DateFormatter.dateFormat; "16-jan-2020" //Specify your format that you want
-                    //let strDate = DateFormatter.string(from: Date)
+                    //
+                    let minutely = jsonDictionary["minutely"] as? [String : Any]
+                    let dataArray = minutely?["data"] as! NSArray
+                    var i = 0
+                    var count = 0
+                    var sum = 0 as Double
+                    for _ in dataArray {
+                        
+                        //print(item as Any)//print(dataArray[i])
+                        
+                        let item = dataArray[i] as! [String: Any]
+                        let precipProb = item["precipProbability"] as! Double
+                        if precipProb != 0 {
+                            sum = sum + precipProb
+                            count+=1
+                        }
+                        //print("i = \(i)")
+                        i+=1
+                    }
+                    let avg = sum / Double(count)
+                    print("Average = \(avg)")
+
+                   // if let minutelyDictionary = jsonDictionary["minutely"] as? [String : Any]{
+                        
+                      //  if let minData = minutelyDictionary["data"] as?  [String : Any ]{
+                            //var arr = [String]()
+                            
+                           // for (key, value) in minData {
+                                //arr.append("\(key) \(value)")
+                          //      print(arr)
+                           // }
+                      //  }
+                        // print(minutelyDictionary)
+                    //}
+                
+                    self.textview.text = strDate
                     
-                   // self.txtview.text = summary
-                   // print(currentWeatherDictionary)
-                    
-                    self.txtview.text = strDate
-                    
+                    self.txtview.text = String (avg)
                     print("=====")
                     
                     print(strDate)
@@ -70,6 +97,8 @@ class ViewController: UIViewController {
                 //    print(jsonDictionary)
                     
                 }
+                
+                
             }
         }
     }
